f1ff77303523768f174bce5441eedff162cf5173
Jacob RAwling <mrjacob3@gmail.com>
2013-04-24 13:10:33 +0100

    hh
    hh

--- main_adaptive.cpp
+++ main_adaptive.cpp
@@ -2,7 +2,7 @@
 //
 //Author: Jacob Rawling
 //Date:	05/02/2013
-//
+//http://www.pa.msu.edu/~mmoore/Lect12_Wavepacket.pdf
 //---------------------------------------------------------------------
 //
 //Aims:
@@ -17,7 +17,7 @@
 //
 //This Document:
 //	1. Main entry point of programme
-//	2. Handling wavefunction as n dimnsional vector, where n is number
+//	2. Handling wavefunction aC s n dimnsional vector, where n is number
 //	   of segments of space. 
 //
 //=====================================================================
@@ -56,19 +56,24 @@ int main(){
 	ofstream output_file, error_file;
 	output_file.precision(5); //output to 5 sig fig
 	error_file.precision(5);
-	error_file.open("C:\\Users\\Jacob\\Documents\\Theory Computing\\Schroedinger WE\\test2\\error.txt");
+	error_file.open("C:\\Users\\Jacob\\Documents\\Theory Computing\\Schroedinger WE\\RESULT\\tunel\\error.txt");
 
 	//define time step and range  
 	double t = 0;										//current place in time
 	double T = 1;										//conversion to dimesionless time 
 	double dt = 5e-4;									//time step
-	double no_steps = 200000;								//60000gives a nice complete picture of what happens
+	double no_steps = 5000;								//60000gives a nice complete picture of what happens
 						
 	//wavefunction
 	vector<complex<double>> psi;
 	psi = psi_initial();	
 	int output_mod = 10;
 	double error = 0.0;
+	double fixed_divisor = 0.0;
+
+	for(int i = 1;i < no_dimensions-1;i++)
+		fixed_divisor += abs(psi[i])*abs(psi[i]);
+
 
 	//perform runge-kutta iterations and output results
 	for(int i = 0; i < no_steps; i++){
@@ -78,18 +83,25 @@ int main(){
 
 		//output previous iteration
 		if(i%output_mod == 0){ //every n steps
-			name << "C:\\Users\\Jacob\\Documents\\Theory Computing\\Schroedinger WE\\test2\\time" << i << ".txt";
+			//Linear Barrier
+			name << "C:\\Users\\Jacob\\Documents\\Theory Computing\\Schroedinger WE\\RESULT\\tunel\\time" << i << ".txt";
 			output_file.open(name.str());	
-			for(int j = 0; j < no_dimensions;j++) //output the probability density 
-				output_file  << j << "\t" << abs(psi[j])*abs(psi[j]) << "\t" << (Potential(t, j*grid_width)/100.0) << "\n";
+			error = 0.0f;
 
+			for(int j = 0; j < no_dimensions;j++){ //output the probability density 
+				output_file  << j << "\t" << abs(psi[j])*abs(psi[j]) << "\t" << (Potential(t, j*grid_width)/100.0) << "\n";
+				if(j > 0 && j < no_dimensions-1)
+					//trapezium rule for integration
+					error += abs(psi[j])*abs(psi[j]);
+			}
+			error /= (fixed_divisor);
+			error_file << error << endl;
 			output_file.close();
 		}//close if i%
 
-		//iterate over a step
-		//psi = BSRK4_step_adaptive(ModifiedHamiltonian,dt,t,psi,error);
-		psi = RK4_step(ModifiedHamiltonian,dt,t,psi);
-		//error_file << error << "\t" << dt << "\t" << t << "\t" << endl;
+		//iterate	over a step
+		psi = RK4_step(ModifiedHamiltonian,dt,t,psi);	
+
 		//increase the time
 		t += dt;
 	}//end for
@@ -127,11 +139,11 @@ vector<complex<double>> psi_initial(){
 	vector<complex<double>> psi(no_dimensions);
 
 	//constants
-	double wdth = 0.8;											//width of the gaussian
+	double wdth = 0.5;  												//width of the gaussian
 	complex<double> total_width = (no_dimensions*grid_width)/a;	//total width of the system
-	complex<double> x_0 = total_width/3.0;						//inital displacement from origin
-	complex<double> Amplitude = 0.8;							//normalization perhaps?..
-	complex<double> p_0		  = 15.0;							//momentum
+	complex<double> x_0 = total_width/2.5;						//inital displacement from origin
+	complex<double> Amplitude = 1.0/(sqrt(2.0*pi*wdth));		//normalization 
+	complex<double> p_0		  = 10.0F;							//momentum
 
 	//create and fill the vector with approriate values	
 	for(int i = 0; i < no_dimensions;i++){
@@ -143,15 +155,32 @@ vector<complex<double>> psi_initial(){
 		psi[0]				 = 0.0;
 		psi[no_dimensions-1] = 0.0;
 	}
+
 	return psi;
 }//close psi_initial
 
 //defines the real potential field that the system is in.
 double Potential(double t, double x){
-	if(x >= 7*a
-		&& x <= 9*a)
-		return 90;
+	//return 0;
+
+	//barrier at x = 1 and 1.8 
+	if(x >= 7.0*a 
+		&& x <= 7.8*a)
+		return 51;
+	return 0;
+
+	//step
+	if(x >= 5.0*a){
+		if(x >= 6.68*a){
+			if(x >= 8.33333333*a)
+				return 80;
+			return 60;
+		}
+		return 40;
+	}
+
 	//return 0.99;
-	//return 5*(((x-(grid_width*no_dimensions/2.0))*(x-(grid_width*no_dimensions/2.0)))/((grid_width*no_dimensions/(2.0))*(grid_width*no_dimensions/2.0)));
+
+	//return 180*(((x-(grid_width*no_dimensions/2.0))*(x-(grid_width*no_dimensions/2.0)))/((grid_width*no_dimensions/(2.0))*(grid_width*no_dimensions/2.0)));
 	return 0;	
-}//close Potential	
+}//close Potential				
